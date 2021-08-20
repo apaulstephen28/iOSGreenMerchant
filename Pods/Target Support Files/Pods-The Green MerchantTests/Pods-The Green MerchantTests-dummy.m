7e4e5543c8a7f83bf8007302079cdbd5536a59db
apaulstephen28 <53103235+apaulstephen28@users.noreply.github.com>
2021-08-21 04:25:18 +0800

    Add files via upload

---
+++ Pods/Target Support Files/Pods-The Green MerchantTests/Pods-The Green MerchantTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_The_Green_MerchantTests : NSObject
+@end
+@implementation PodsDummy_Pods_The_Green_MerchantTests
+@end
---
+++ Pods/Target Support Files/Pods-The Green MerchantUITests/Pods-The Green MerchantUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_The_Green_MerchantUITests : NSObject
+@end
+@implementation PodsDummy_Pods_The_Green_MerchantUITests
+@end
