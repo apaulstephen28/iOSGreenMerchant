62b45341401464d1a01a9b6d4b6bbe88302efa9a
Paul Stephen <53103235+apaulstephen28@users.noreply.github.com>
2021-08-21 04:56:54 +0800

    Add files via upload

---
+++ The Green MerchantTests/The_Green_MerchantTests.swift
@@ -0,0 +1,34 @@
+//
+//  The_Green_MerchantTests.swift
+//  The Green MerchantTests
+//
+//  Created by Paul Austria on 01/03/2019.
+//  Copyright © 2019 Paul Austria. All rights reserved.
+//
+
+import XCTest
+@testable import The_Green_Merchant
+
+class The_Green_MerchantTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
